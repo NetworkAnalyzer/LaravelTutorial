71fb3188a9e9dbb1e23804ff15924285d5a0948b
Daiki <nojiri1098@gmail.com>
2018-08-11 13:38:39 +0900

    [add] 選択した画像のプレビューを表示

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.layout')
 
-@section('styles')
-
-    <link href="{{ asset('css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css') }}" rel="stylesheet">
-
-@endsection
-
 @section('content')
 
     <div class="wrapper wrapper-content">
@@ -20,6 +14,7 @@
                             <div class="form-control">
                                 <div class="col-lg-10">
                                     {{ Form::file('image') }}
+                                    <div class="preview" />
                                 </div>
                             </div>
                             <div class="col-lg-offset-2 col-lg-10">
@@ -36,15 +31,39 @@
 
 @section('scripts')
 
-    <!-- iCheck -->
-    <script src="{{ asset('js/plugins/iCheck/icheck.min.js') }}"></script>
     <script>
-        $(document).ready(function () {
-            $('.i-checks').iCheck({
-                checkboxClass: 'icheckbox_square-green',
-                radioClass: 'iradio_square-green',
+        $(function(){
+            $('form').on('change', 'input[type="file"]', function(e) {
+                var file = e.target.files[0],
+                    reader = new FileReader(),
+                    $preview = $(".preview");
+                t = this;
+
+                // exit processing if a file is not image
+                if(file.type.indexOf("image") < 0){
+                    return false;
+                }
+
+                // display the loaded image
+                reader.onload = (function(file) {
+                    return function(e) {
+                        console.log(e);
+                        // remove existing preview
+                        $preview.empty();
+                        // add <image> in the <div> has preview class
+                        $preview.append($('<img>').attr({
+                            src: e.target.result,
+                            width: "150px",
+                            class: "preview",
+                            title: file.name
+                        }));
+                    };
+                })(file);
+
+                reader.readAsDataURL(file);
             });
         });
+
     </script>
 
 @endsection
